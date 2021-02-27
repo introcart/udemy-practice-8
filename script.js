ec243a6bb0d5e2d8cddc451eb491b42fbb622ab0
Steven Carton <spatc@protonmail.com>
2021-02-27 17:23:08 -0500

    updating this information

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ function calcAge(birthYear) {
 
     if (birthYear >= 1981 && birthYear <= 1996) {
       var millenial = true;
+      const firstName = 'Steven';
       const str = `oh, and you're a millenial, ${firstName}`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
     }
     // console.log(str);
     console.log(millenial);
+    // add(2, 3);
   }
   printage();
   return age;
@@ -23,3 +29,16 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printage();
+
+// this keyword difference between regular functions and arrow functions
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+jonas.greet();
