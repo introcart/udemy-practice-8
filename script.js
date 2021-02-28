4ade29e3e93ad2c1548271699e0e0f2890640e94
Steven Carton <spatc@protonmail.com>
2021-02-27 23:11:46 -0500

    final push

--- script.js
+++ script.js
@@ -61,3 +61,16 @@ const jonas = {
 };
 jonas.greet();
 jonas.calcAge();
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 9);
